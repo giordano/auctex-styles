bc200939a477aab56cd28a188b54e48f27e7aaa1
Mosè Giordano <giordano.mose@libero.it>
2012-11-07 15:51:30 +0100

    bm.el: added DeclareBoldMathCommand, bmdefine and hmdefine macros

--- bm.el
+++ bm.el
@@ -14,13 +14,20 @@
  (lambda ()
    (TeX-add-symbols
     '("bm" 1)
-    '("hm" 1))
+    '("hm" 1)
+    '("DeclareBoldMathCommand" [ "Math version" ] TeX-arg-define-macro "Math expression")
+    '("bmdefine" TeX-arg-define-macro "Math expression")
+    '("hmdefine" TeX-arg-define-macro "Math expression"))
    ;; Fontification
    (when (and (featurep 'font-latex)
    	      (eq TeX-install-font-lock 'font-latex-setup))
      (font-latex-add-keywords '(("bm" "{")
-   				("hm" "{"))
-   			      'bold-command))))
+				("hm" "{"))
+			      'bold-command)
+     (font-latex-add-keywords '(("DeclareBoldMathCommand" "[|{\\{")
+				("bmdefine" "|{\\{")
+				("hmdefine" "|{\\{"))
+			      'function))))
 
 (defvar LaTeX-bm-package-options nil
   "Package options for the bm package.")
