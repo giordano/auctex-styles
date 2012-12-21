bfd87ac838655e1ce9980616cef8ed6f31e65576
Mosè Giordano <giordano.mose@libero.it>
2012-12-21 17:59:44 +0100

    bm.el and siunitx.el now are part of AUCTeX

--- bm.el
+++ bm.el
@@ -1,8 +1,27 @@
-;;; bm.el --- AUCTeX style for `bm.sty'
+;;; bm.el --- AUCTeX style for `bm.sty'.
 
+;; Copyright (C) 2012 Free Software Foundation, Inc.
+
+;; Maintainer: auctex-devel@gnu.org
 ;; Author: Mosè Giordano <giordano.mose@libero.it>
-;; Created: 2012-10-22
 ;; Keywords: tex
+
+;; This file is part of AUCTeX.
+
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; AUCTeX is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with AUCTeX; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
 
 ;;; Commentary:
 
